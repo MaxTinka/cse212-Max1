f76d83da76eca58ea11e12faf56839fb24ecc435
MaxTinka <tinkamax03@gmail.com>
2026-01-24 11:38:36 +0300

    Refactor TakingTurnsQueue and remove Person class
    Refactored Person class and updated queue logic.

--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -1,21 +1,9 @@
 using System;
 using System.Collections.Generic;
 
-public class Person
-{
-    public string Name { get; set; }
-    public int Turns { get; set; }
-
-    public Person(string name, int turns)
-    {
-        Name = name;
-        Turns = turns;
-    }
-}
-
 public class TakingTurnsQueue
 {
-    private readonly Queue<Person> _queue = new Queue<Person>();
+    private readonly Queue<Person> _queue = new();
     
     public int Length => _queue.Count;
 
@@ -29,19 +17,15 @@ public class TakingTurnsQueue
         if (_queue.Count == 0)
             throw new InvalidOperationException("No one in the queue.");
 
-        // Get the person at the front
         Person person = _queue.Dequeue();
         
-        // Handle turn logic
-        if (person.Turns > 1)
+        if (person.Turns > 1) // Finite turns and will have turns left after this one
         {
-            // Finite turns and still has turns left after this one
             person.Turns--;
             _queue.Enqueue(person);
         }
-        else if (person.Turns <= 0)
+        else if (person.Turns <= 0) // Infinite turns (0 or negative)
         {
-            // Infinite turns (0 or negative) - re-add with same turn count
             _queue.Enqueue(person);
         }
         // If Turns == 1, don't re-add (they've used their last turn)
